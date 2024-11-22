306e368c400a46bb529697e71a8ed7acbf135d25
Siddhanth Sridhar <87889951+sidd2305@users.noreply.github.com>
2024-11-22 16:33:36 +0530

    Update main.py

--- Code/main.py
+++ Code/main.py
@@ -437,4 +437,5 @@ if __name__ == "__main__":
             r"C:/Program Files/Tesseract-OCR/tesseract.exe"
         )
 
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
